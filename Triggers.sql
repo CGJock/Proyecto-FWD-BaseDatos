36a2f5ccf84e727212f84edf7d6a4a3bc72a7079
RaulEV1411 <136620455+RaulEV1411@users.noreply.github.com>
2024-08-26 16:48:45 -0600

    Trigger estado habitacion

---
+++ Triggers.sql
@@ -0,0 +1,12 @@
+USE Reservations_data;
+
+CREATE TRIGGER actualizar_estado_habitacion
+AFTER INSERT ON Room_reservation
+FOR EACH ROW
+BEGIN
+    UPDATE Rooms
+    SET occupied = TRUE
+    WHERE room_id = NEW.room_id;
+END;
+
+DROP TRIGGER actualizar_estado_habitacion;
