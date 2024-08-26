d98c2c5deb082fa335a9ebf5615148f70f5a1db7
CGJock <cjocksanest@gmail.com>
2024-08-26 11:48:33 -0600

    creacion de base de datos y tablas

--- ProyectoAdministracion.sql
+++ ProyectoAdministracion.sql
@@ -5,40 +5,81 @@ USE Reservations_data;
 
 CREATE TABLE Roles(
     role_id INT PRIMARY KEY AUTO_INCREMENT,
-    name_role VARCHAR(25)
+    name_role VARCHAR(25) NOT NULL UNIQUE
 );
 
 CREATE TABLE Subscription(
     sub_id INT PRIMARY KEY AUTO_INCREMENT,
-    sub_type VARCHAR(25)
+    sub_type VARCHAR(25) NOT NULL UNIQUE
 );
 
 CREATE Table Pay_method(
     pay_id INT PRIMARY KEY AUTO_INCREMENT,
-    pay_type VARCHAR(25)
+    pay_type VARCHAR(25) NOT NULL UNIQUE
 );
 
 CREATE TABLE Users(
-    user_id INT,
-    role_id INT ,
-    sub_id  INT ,
-    pay_id INT,
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    sub_id  INT NOT NULL,
+    pay_id INT NOT NULL,
     first_name VARCHAR(25),
     middle_name VARCHAR(25),
     last_name VARCHAR(25),
-    email VARCHAR(25) UNIQUE,
+    email VARCHAR(25) CHECK(email LIKE '%@%.%') UNIQUE,
     birth_date DATE,
     PRIMARY KEY(user_id),
-    FOREIGN KEY(role_id) INT REFERENCES Roles(role_id),
-    FOREIGN KEY (sub_id)  INT REFERENCES Subcription(sub_id),
-    FOREIGN KEY(pay_id) INT REFERENCES Pay_method(pay_id)
+    FOREIGN KEY(role_id)  REFERENCES Roles(role_id) ON DELETE CASCADE,
+    FOREIGN KEY (sub_id)  REFERENCES Subscription(sub_id) ON DELETE CASCADE,
+    FOREIGN KEY(pay_id) REFERENCES Pay_method(pay_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Hotels(
     hotel_id INT AUTO_INCREMENT,
-    hotel_name VARCHAR(25),
-    location VARCHAR(25),
-    rooms_number INT,
+    hotel_name VARCHAR(25) NOT NULL,
+    location VARCHAR(25) NOT NULL,
+    rooms_number INT CHECK(rooms_number > 0) NOT NULL,
     PRIMARY KEY(hotel_id)
 )
+
+CREATE TABLE Type_room(
+    type_id INT AUTO_INCREMENT,
+    category VARCHAR(25) NOT NULL,
+    capacity INT CHECK(capacity > 0) NOT NULL,
+    cost_per_night INT CHECK(cost_per_night >= 0)NOT NULL,
+    PRIMARY KEY(type_id)
+)
+
+CREATE TABLE Rooms(
+    room_id INT,
+    room_number INT NOT NULL,
+    hotel_id INT NOT NULL,
+    type_id INT NOT NULL,
+    occupied BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY(room_id),
+    FOREIGN KEY(hotel_id) REFERENCES Hotels(hotel_id) ON DELETE CASCADE,
+    FOREIGN key(type_id) REFERENCES Type_room(type_id) ON DELETE CASCADE
+)
+
+CREATE TABLE Reservations(
+    reservation_id INT,
+    user_id INT NOT NULL,
+    check_in_date DATE NOT NULL,
+    check_out_date DATE NOT NULL,
+    number_visitors INT CHECK(number_visitors > 0) NOT NULL,
+    total_price INT CHECK(total_price >= 0)  NOT NULL,
+    PRIMARY KEY(reservation_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+    
+);
+
+CREATE TABLE Room_reservation(
+    reservation_id INT NOT NULL,
+    room_id INT NOT NULL,
+    FOREIGN KEY(reservation_id) REFERENCES Reservations(reservation_id) ON DELETE CASCADE,
+    FOREIGN KEY(room_id) REFERENCES Rooms(room_id) ON DELETE CASCADE
+    
+);
+
+DROP DATABASE reservations_data;
 
