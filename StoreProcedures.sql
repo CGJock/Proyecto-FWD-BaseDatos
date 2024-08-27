996543f5cbacb2cf2245bd371e737a8c5e77dfb7
CGJock <cjocksanest@gmail.com>
2024-08-26 21:12:02 -0600

    correcciones en el procedure

--- StoreProcedures.sql
+++ StoreProcedures.sql
@@ -4,28 +4,30 @@ USE Reservations_data;
 DELIMITER//
 
 CREATE PROCEDURE consult_room_by_date(
-IN room_id INT,
-IN check_out_date DATE,
-IN check_in_date DATE,
+IN room_book_id INT,
 IN actual_date DATE,
-OUT check_room BOOL
+OUT avaliable_room BOOL
 )
 BEGIN 
-    DECLARE room_aviavility INT;
-    SELECT COUNT(*) INTO room_aviavility
-    FROM rooms 
-    WHERE (actual_date > check_out_date) 
-    AND (actual_date < check_in_date);
-    
-    IF room_aviavility > 0 THEN
-        SET check_room = FALSE;
+    IF EXISTS(
+        SELECT 1
+        FROM reservations
+        JOIN rooms ON rooms.room_id 
+        WHERE rooms.room_id = room_id
+        AND
+        actual_date BETWEEN reservations.check_in_date  AND reservations.check_out_date
+    ) THEN
+        SET avaliable_room = FALSE;
     ELSE
-        SET check_room = TRUE;
+        SET avaliable_room = TRUE;
     END IF;
     END//
     DELIMITER ;
 
-    CALL consult_room_by_date(1,'2024-06-15','2024-06-20','2024-06-17', @check_room );
-    SELECT @check_room
+    CALL consult_room_by_date(4,'2024-10-08', @avaliable_room );
+    SELECT @avaliable_room
+
+   
+
 
 
