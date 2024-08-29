9f15ec579036bf3ac213492700578deed00be29d
RaulEV1411 <136620455+RaulEV1411@users.noreply.github.com>
2024-08-29 00:58:31 -0600

    trigger precios reservas

--- Triggers.sql
+++ Triggers.sql
@@ -18,4 +18,30 @@ BEGIN
     WHERE room_id = OLD.room_id;
 END;
 
+DELIMITER //
+CREATE TRIGGER Precio_total_reserva
+AFTER INSERT ON room_reservation
+FOR EACH ROW
+BEGIN
+    DECLARE costo_total DECIMAL(10,2);
+    DECLARE Noches INT;
+
+    SELECT DATEDIFF(reservations.check_out_date, reservations.check_in_date)
+    INTO Noches
+    FROM reservations 
+    WHERE reservations.reservation_id = NEW.reservation_id;
+
+    SELECT SUM(type_room.cost_per_night * Noches)
+    INTO costo_total
+    FROM rooms
+    JOIN type_room ON rooms.type_id = type_room.type_id
+    JOIN room_reservation ON rooms.room_id = room_reservation.room_id
+    WHERE room_reservation.reservation_id = NEW.reservation_id;
+
+    UPDATE reservations
+    SET total_price = costo_total
+    WHERE reservation_id = NEW.reservation_id;
+END//
+DELIMITER ;
+
 DROP TRIGGER actualizar_estado_habitacion;
