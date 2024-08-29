4a6796be20ec03bf2d5e9a3aff79efe438ad0da6
CGJock <cjocksanest@gmail.com>
2024-08-29 11:13:16 -0600

    creacion de vista de usuarios

--- View.sql
+++ View.sql
@@ -6,3 +6,9 @@ SELECT reservation_id,user_id,total_price
 FROM reservations
 WHERE check_in_date = CURRENT_DATE
 
+--view que lista todos los usuarios con el role
+CREATE VIEW user_list AS 
+SELECT users.user_id,users.role_id,users.first_name,users.middle_name,roles.name_role
+FROM users
+JOIN roles ON roles.role_id = users.role_id
+
